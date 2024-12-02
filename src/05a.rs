687b262b928b885d70d9be5ec7060b20ca76a290
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-02 23:04:57 +0000

    template for rest of days

---
+++ src/05a.rs
@@ -0,0 +1,12 @@
+// Advent of Code 2024
+// (c) 2023 Mateusz Kwapich
+
+use anyhow::Result;
+use std::io::BufRead;
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin();
+
+    for _line in stdin.lock().lines() {}
+    Ok(())
+}
