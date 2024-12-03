cd386ba2b365d3338ffb54d78414d45eeaeeb2e2
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-03 05:26:51 +0000

    Day 3

--- src/03a.rs
+++ src/03a.rs
@@ -2,11 +2,21 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use regex::Regex;
 use std::io::BufRead;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut sum: i64 = 0;
+
+    for line in stdin.lock().lines() {
+        let line = line?;
+        for caps in re.captures_iter(&line) {
+            sum += caps[1].parse::<i64>()? * caps[2].parse::<i64>()?;
+        }
+    }
+    println!("{}", sum);
     Ok(())
 }
--- src/03b.rs
+++ src/03b.rs
@@ -2,11 +2,33 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use regex::Regex;
 use std::io::BufRead;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let re = Regex::new(r"(?P<mul>mul\((\d+),(\d+)\))|(?P<dont>don't\(\))|(?P<do>do\(\))").unwrap();
+    let mut sum: i64 = 0;
+
+    let mut toggle = true;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        for caps in re.captures_iter(&line) {
+            if let Some(_link_match) = caps.name("mul") {
+                if toggle {
+                    sum += caps[2].parse::<i64>()? * caps[3].parse::<i64>()?;
+                }
+            }
+
+            if let Some(_link_match) = caps.name("do") {
+                toggle = true;
+            }
+            if let Some(_link_match) = caps.name("dont") {
+                toggle = false;
+            }
+        }
+    }
+    println!("{}", sum);
     Ok(())
 }
