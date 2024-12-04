f1456cca7f10398d1588ffc6b814c23d5b975be2
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-04 05:45:21 +0000

    Day 4
    Summary:
    
    Test Plan:

--- src/04a.rs
+++ src/04a.rs
@@ -2,11 +2,58 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use itertools::Itertools;
 use std::io::BufRead;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut lines = vec![];
+    let mut horizontal = String::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        lines.push(line.chars().collect_vec());
+        horizontal.push_str(&line);
+        horizontal.push('\n');
+    }
+    let h = lines.len();
+    let w = lines[0].len();
+
+    let mut vertical = String::new();
+    for i in 0..h {
+        for line in lines.iter() {
+            vertical.push(line[i]);
+        }
+        vertical.push('\n');
+    }
+
+    let mut diag1 = String::new();
+    for i in 0..(h + w - 1) {
+        for j in (0..=i).rev() {
+            let c = lines.get(j).map(|l| l.get(i - j));
+            if let Some(Some(c)) = c {
+                diag1.push(*c);
+            }
+        }
+        diag1.push('\n');
+    }
+
+    let mut diag2 = String::new();
+    for i in 0..(h + w - 1) {
+        for j in (0..=i).rev() {
+            let c = lines.get(j).map(|l| l.get(w - i + j));
+            if let Some(Some(c)) = c {
+                diag2.push(*c);
+            }
+        }
+        diag2.push('\n');
+    }
+
+    let mut sum = 0;
+    for s in [horizontal, vertical, diag1, diag2] {
+        sum += s.matches("XMAS").count();
+        sum += s.matches("SAMX").count();
+    }
+    println!("{sum}");
     Ok(())
 }
--- src/04b.rs
+++ src/04b.rs
@@ -2,11 +2,45 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use itertools::Itertools;
 use std::io::BufRead;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut lines = vec![];
+    for line in stdin.lock().lines() {
+        let line = line?;
+        lines.push(line.chars().collect_vec());
+    }
+
+    let h = lines.len();
+    let w = lines[0].len();
+
+    let mut sum = 0;
+    for i in 1..(h - 1) {
+        for j in 1..(w - 1) {
+            let mut cnt = 0;
+            if lines[i][j] == 'A' {
+                if lines[i - 1][j - 1] == 'M' || lines[i + 1][j + 1] == 'M' {
+                    cnt += 1;
+                }
+                if lines[i - 1][j - 1] == 'S' || lines[i + 1][j + 1] == 'S' {
+                    cnt += 1;
+                }
+                if lines[i - 1][j + 1] == 'M' || lines[i + 1][j - 1] == 'M' {
+                    cnt += 1;
+                }
+                if lines[i - 1][j + 1] == 'S' || lines[i + 1][j - 1] == 'S' {
+                    cnt += 1;
+                }
+            }
+            if cnt == 4 {
+                sum += 1;
+            }
+        }
+    }
+
+    println!("{sum}");
     Ok(())
 }
