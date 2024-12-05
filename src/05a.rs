e1fa92d31497112c783237cddbb487f696a67e6a
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-05 05:31:22 +0000

    Day 5

--- src/05a.rs
+++ src/05a.rs
@@ -2,11 +2,51 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use std::{collections::BTreeSet, io::BufRead};
+#[macro_use]
+extern crate scan_rules;
+use scan_rules::scan;
+
+fn validate(rules: &BTreeSet<(i64, i64)>, pages: &[i64]) -> bool {
+    for w in pages.windows(2) {
+        let (a, b) = (w[0], w[1]);
+        if rules.contains(&(b, a)) {
+            return false;
+        }
+    }
+    true
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut rules = BTreeSet::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+
+        scan!(&line;
+            (let num_a: i64, "|", let num_b: i64) => {
+                rules.insert((num_a, num_b));
+            },
+        )
+        .unwrap();
+    }
+
+    let mut sum = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let pages = line
+            .split(',')
+            .map(|n| n.parse::<i64>())
+            .collect::<Result<Vec<_>, std::num::ParseIntError>>()?;
+
+        if validate(&rules, &pages) {
+            sum += pages[pages.len() / 2];
+        }
+    }
+    println!("{sum}");
     Ok(())
 }
--- src/05b.rs
+++ src/05b.rs
@@ -2,11 +2,63 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use std::{cmp::Ordering, collections::BTreeSet, io::BufRead};
+#[macro_use]
+extern crate scan_rules;
+use scan_rules::scan;
+
+fn validate(rules: &BTreeSet<(i64, i64)>, pages: &[i64]) -> bool {
+    for w in pages.windows(2) {
+        let (a, b) = (w[0], w[1]);
+        if rules.contains(&(b, a)) {
+            return false;
+        }
+    }
+    true
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut rules = BTreeSet::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+
+        scan!(&line;
+            (let num_a: i64, "|", let num_b: i64) => {
+                rules.insert((num_a, num_b));
+            },
+        )
+        .unwrap();
+    }
+
+    let mut sum = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let mut pages = line
+            .split(',')
+            .map(|n| n.parse::<i64>())
+            .collect::<Result<Vec<_>, std::num::ParseIntError>>()?;
+
+        if validate(&rules, &pages) {
+            // skip over correctly validating pairs
+            continue;
+        }
+
+        pages.sort_by(|a, b| {
+            if rules.contains(&(*a, *b)) {
+                return Ordering::Less;
+            }
+            if rules.contains(&(*b, *a)) {
+                return Ordering::Greater;
+            }
+            a.cmp(b)
+        });
+        sum += pages[pages.len() / 2];
+    }
+    println!("{sum}");
     Ok(())
 }
