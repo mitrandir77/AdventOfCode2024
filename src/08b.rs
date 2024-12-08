0c0989a2fdbdcce9c677422104a89d010a92ee56
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-08 08:18:13 +0000

    Day 8

--- src/08b.rs
+++ src/08b.rs
@@ -2,11 +2,83 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use btreemultimap::BTreeMultiMap;
+use std::{collections::BTreeSet, io::BufRead};
+
+#[allow(dead_code)]
+fn print_map(map: &[Vec<u8>], antinodes: &BTreeSet<(usize, usize)>) {
+    let mut map = map.to_vec();
+
+    for (y, row) in map.iter_mut().enumerate() {
+        for (x, c) in row.iter_mut().enumerate() {
+            if antinodes.contains(&(y, x)) {
+                *c = b'#';
+            }
+        }
+    }
+
+    for line in map.iter() {
+        let s = String::from_utf8_lossy(line);
+        eprintln!("{}", s);
+    }
+}
+
+fn count_antinodes(map: &mut [Vec<u8>]) -> usize {
+    let mut antinodes = BTreeSet::new();
+    let mut frequency_map = BTreeMultiMap::new();
+    let h = map.len();
+    let w = map[0].len();
+
+    for (y, row) in map.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if *c != b'.' {
+                frequency_map.insert(*c, (y, x));
+            }
+        }
+    }
+
+    for freq in frequency_map.keys() {
+        // dbg!(*freq as char);
+        for (ay, ax) in frequency_map.get_vec(freq).unwrap() {
+            for (by, bx) in frequency_map.get_vec(freq).unwrap() {
+                if ay == by && ax == bx {
+                    continue;
+                }
+                let (xd, yd) = (ax - bx, ay - by);
+
+                for mul in 0.. {
+                    let (ny, nx) = (ay + yd * mul, ax + xd * mul);
+                    if !((0..h).contains(&ny) && (0..w).contains(&nx)) {
+                        break;
+                    }
+                    antinodes.insert((ny, nx));
+                }
+                for mul in 0.. {
+                    let (ny, nx) = (by - yd * mul, bx - xd * mul);
+                    if !((0..h).contains(&ny) && (0..w).contains(&nx)) {
+                        break;
+                    }
+                    antinodes.insert((ny, nx));
+                }
+            }
+        }
+    }
+    // print_map(map, &antinodes);
+
+    antinodes.len()
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut map = vec![];
+    for line in stdin.lock().lines() {
+        let line = line?;
+        map.push(line.as_bytes().to_vec());
+    }
+    let sum = count_antinodes(&mut map);
+    // print_map(&map);
+    println!("{sum}");
+
     Ok(())
 }
