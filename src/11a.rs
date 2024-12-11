e37cd876710af987933ab522e9e10e315ab0ee52
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-11 07:31:07 +0000

    Day 11

--- src/11a.rs
+++ src/11a.rs
@@ -4,9 +4,46 @@
 use anyhow::Result;
 use std::io::BufRead;
 
+fn blink(stones: &[u64]) -> Vec<u64> {
+    let mut res = vec![];
+
+    for stone in stones {
+        if *stone == 0 {
+            res.push(1);
+            continue;
+        }
+
+        let digits = stone.to_string();
+        let len = digits.len();
+        if len % 2 == 0 {
+            let left = digits[0..len / 2].parse().unwrap();
+            let right = digits[len / 2..len].parse().unwrap();
+            res.push(left);
+            res.push(right);
+            continue;
+        }
+
+        res.push(*stone * 2024);
+    }
+    res
+}
+
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut stones = vec![];
+    for line in stdin.lock().lines() {
+        let line = line?;
+        for num in line.split(" ") {
+            let num: u64 = num.parse()?;
+            stones.push(num);
+        }
+    }
+
+    for _i in 0..25 {
+        stones = blink(&stones);
+    }
+
+    println!("{}", stones.len());
     Ok(())
 }
--- src/11b.rs
+++ src/11b.rs
@@ -2,11 +2,48 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use memoize::memoize;
 use std::io::BufRead;
+
+#[memoize]
+fn num_stones(stone: u64, iter: usize) -> usize {
+    if iter == 0 {
+        return 1;
+    }
+
+    if stone == 0 {
+        return num_stones(1, iter - 1);
+    }
+
+    let digits = stone.to_string();
+    let len = digits.len();
+    if len % 2 == 0 {
+        let left = digits[0..len / 2].parse().unwrap();
+        let right = digits[len / 2..len].parse().unwrap();
+
+        return num_stones(left, iter - 1) + num_stones(right, iter - 1);
+    }
+
+    num_stones(stone * 2024, iter - 1)
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut stones = vec![];
+    for line in stdin.lock().lines() {
+        let line = line?;
+        for num in line.split(" ") {
+            let num: u64 = num.parse()?;
+            stones.push(num);
+        }
+    }
+
+    let mut res = 0;
+    for stone in stones {
+        res += num_stones(stone, 75);
+    }
+
+    println!("{}", res);
     Ok(())
 }
