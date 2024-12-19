b40916ce1350e85b4a2589be3b7cb1c19b17280d
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-19 23:30:23 +0100

    Day 19

--- src/19a.rs
+++ src/19a.rs
@@ -2,11 +2,43 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use scan_rules::scanner::Word;
 use std::io::BufRead;
+#[macro_use]
+extern crate scan_rules;
+
+fn possible(towels: &[String], design: &str) -> bool {
+    let mut cache = vec![false; design.len() + 1];
+    cache[design.len()] = true;
+
+    'outer: for start in (0..design.len()).rev() {
+        for towel in towels {
+            // dbg!(&design[start..]);
+            if design[start..].starts_with(towel) && cache[start + towel.len()] {
+                cache[start] = true;
+                continue 'outer;
+            }
+        }
+    }
+    // dbg!(&cache);
+    cache[0]
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut lines = stdin.lock().lines();
+    let line = lines.next().unwrap()?;
+    let_scan!(line; ([ let towels: Word<String>],+));
+
+    let mut sum = 0;
+    for line in lines.skip(1) {
+        let design = line?;
+        if possible(&towels, &design) {
+            sum += 1;
+        }
+    }
+    println!("{sum}");
+
     Ok(())
 }
--- src/19b.rs
+++ src/19b.rs
@@ -2,11 +2,40 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
+use scan_rules::scanner::Word;
 use std::io::BufRead;
+#[macro_use]
+extern crate scan_rules;
+
+fn ways_to_arrange(towels: &[String], design: &str) -> u64 {
+    let mut cache = vec![0; design.len() + 1];
+    cache[design.len()] = 1;
+
+    for start in (0..design.len()).rev() {
+        for towel in towels {
+            // dbg!(&design[start..]);
+            if design[start..].starts_with(towel) {
+                cache[start] += cache[start + towel.len()];
+            }
+        }
+    }
+    // dbg!(&cache);
+    cache[0]
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut lines = stdin.lock().lines();
+    let line = lines.next().unwrap()?;
+    let_scan!(line; ([ let towels: Word<String>],+));
+
+    let mut sum = 0;
+    for line in lines.skip(1) {
+        let design = line?;
+        sum += ways_to_arrange(&towels, &design);
+    }
+    println!("{sum}");
+
     Ok(())
 }
