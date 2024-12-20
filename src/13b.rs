b631b699cd88a66809fabdfce5aac9c7e9ef361a
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-21 00:03:47 +0100

    Day 13 - part 2
    What seemed like number theory turned out to be simple linear algebra.

--- src/13b.rs
+++ src/13b.rs
@@ -3,10 +3,39 @@
 
 use anyhow::Result;
 use std::io::BufRead;
+#[macro_use]
+extern crate scan_rules;
+use scan_rules::scan;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut lines_iter = stdin.lock().lines();
+    let mut sum = 0;
+    while let Some(line) = lines_iter.next() {
+        let line = line?;
+        let_scan!(line; ("Button A: X+", let ax: i64, ", Y+", let ay: i64));
+        let line = lines_iter.next().unwrap()?;
+        let_scan!(line; ("Button B: X+", let bx: i64, ", Y+", let by: i64));
+        let line = lines_iter.next().unwrap()?;
+        let_scan!(line; ("Prize: X=", let px: i64, ", Y=", let py: i64));
+        lines_iter.next();
+
+        let px = px + 10000000000000;
+        let py = py + 10000000000000;
+
+        // println!("A({ax}, {ay}) B({bx},{by}) P({px},{py})");
+
+        // ax * a + bx * b = px;
+        // ay * a + by * b = py;
+        let b = (ax * py - ay * px) / (ax * by - ay * bx);
+        let a = (px - bx * b) / ax;
+
+        if ax * a + bx * b == px && ay * a + by * b == py {
+            sum += 3 * a + b;
+        }
+    }
+
+    println!("{sum}");
     Ok(())
 }
