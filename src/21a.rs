241ca9301f47bd0b5d7837fba0433fbe4993a46e
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-23 00:03:04 +0100

    Day 21 - part 1

--- src/21a.rs
+++ src/21a.rs
@@ -2,11 +2,116 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use std::{collections::VecDeque, io::BufRead};
+#[macro_use]
+extern crate scan_rules;
+
+pub mod map;
+use map::*;
+
+fn shortest_paths(map: &Map, start: u8, end: u8) -> Vec<Vec<u8>> {
+    // dbg!(start as char, end as char);
+    let mut res = vec![];
+    let start = map.all_points().find(|(_p, v)| *v == start).unwrap().0;
+    let end = map.all_points().find(|(_p, v)| *v == end).unwrap().0;
+
+    let mut queue = VecDeque::new();
+    let mut min_dist = None;
+    queue.push_back((start, 0, vec![]));
+
+    while let Some((p, dist, dirs)) = queue.pop_front() {
+        if let Some(min_dist) = min_dist {
+            if dist > min_dist {
+                break;
+            }
+        }
+        if p == end {
+            min_dist = Some(dist);
+            res.push(encode(&dirs));
+        }
+        for (np, nd) in p.neighbours() {
+            if let Some(nval) = map.get(&np) {
+                if nval == b' ' {
+                    continue;
+                }
+                let mut ndirs = dirs.clone();
+                ndirs.push(nd);
+                queue.push_back((np, dist + 1, ndirs));
+            }
+        }
+    }
+    res
+}
+
+fn encode(dirs: &[Dir]) -> Vec<u8> {
+    dirs.iter()
+        .map(|d| match d {
+            Dir::Up => b'^',
+            Dir::Down => b'v',
+            Dir::Left => b'<',
+            Dir::Right => b'>',
+        })
+        .chain(Some(b'A'))
+        .collect()
+}
+
+const NUMPAD_STR: &str = "\
+789
+456
+123
+ 0A";
+const DIRPAD_STR: &str = " ^A
+<v>";
+
+lazy_static! {
+    static ref NUMPAD: Map = Map::new(
+        NUMPAD_STR
+            .lines()
+            .map(|l| l.bytes().collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+    );
+    static ref DIRPAD: Map = Map::new(
+        DIRPAD_STR
+            .lines()
+            .map(|l| l.bytes().collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+    );
+}
+
+fn inception(input: &[u8], pads: &[&Map]) -> Vec<u8> {
+    if pads.is_empty() {
+        return input.to_vec();
+    }
+    let mut res = vec![];
+    for (s, e) in Some(b'A')
+        .into_iter()
+        .chain(input.iter().cloned())
+        .tuple_windows()
+    {
+        let paths = shortest_paths(pads[0], s, e);
+        let best = paths
+            .iter()
+            .map(|path| inception(path, &pads[1..]))
+            .min_by(|a, b| a.len().cmp(&b.len()))
+            .unwrap();
+        res.extend(best);
+    }
+    res
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut res = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let best = inception(line.as_bytes(), &[&NUMPAD, &DIRPAD, &DIRPAD]);
+
+        let_scan!(line; (let num: usize, "A"));
+        res += num * best.len();
+    }
+    println!("{res}");
     Ok(())
 }
---
+++ src/map.rs
@@ -0,0 +1,122 @@
+// Advent of Code 2024
+// (c) 2024 Mateusz Kwapich
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Dir {
+    pub fn clockwise_rotate(&self) -> Self {
+        match self {
+            Dir::Up => Dir::Right,
+            Dir::Right => Dir::Down,
+            Dir::Down => Dir::Left,
+            Dir::Left => Dir::Up,
+        }
+    }
+
+    pub fn counter_clockwise_rotate(&self) -> Self {
+        match self {
+            Dir::Up => Dir::Left,
+            Dir::Right => Dir::Up,
+            Dir::Down => Dir::Right,
+            Dir::Left => Dir::Down,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+
+    pub fn neighbours(&self) -> [(Point, Dir); 4] {
+        [
+            (Self::new(self.x + 1, self.y), Dir::Right),
+            (Self::new(self.x, self.y - 1), Dir::Up),
+            (Self::new(self.x, self.y + 1), Dir::Down),
+            (Self::new(self.x - 1, self.y), Dir::Left),
+        ]
+    }
+
+    pub fn step(&self, dir: Dir) -> Point {
+        match dir {
+            Dir::Up => Self::new(self.x, self.y - 1),
+            Dir::Right => Self::new(self.x + 1, self.y),
+            Dir::Down => Self::new(self.x, self.y + 1),
+            Dir::Left => Self::new(self.x - 1, self.y),
+        }
+    }
+}
+
+pub struct Map {
+    tiles: Vec<Vec<u8>>,
+    h: usize,
+    w: usize,
+}
+
+impl Map {
+    pub fn new(tiles: Vec<Vec<u8>>) -> Self {
+        let h = tiles.len();
+        let w = tiles[0].len();
+        Map { tiles, h, w }
+    }
+
+    pub fn get(&self, p: &Point) -> Option<u8> {
+        let Ok(x) = usize::try_from(p.x) else {
+            return None;
+        };
+        let Ok(y) = usize::try_from(p.y) else {
+            return None;
+        };
+        let res = self.tiles.get(y).and_then(|row| row.get(x).copied());
+        res
+    }
+
+    pub fn set(&mut self, p: &Point, val: u8) {
+        self.tiles[p.y as usize][p.x as usize] = val
+    }
+
+    pub fn set2(&mut self, p: &Point, val: &[u8; 2]) {
+        self.tiles[p.y as usize][p.x as usize] = val[0];
+        self.tiles[p.y as usize][p.x as usize + 1] = val[1];
+    }
+
+    pub fn contains(&self, p: &Point) -> bool {
+        let Ok(x) = usize::try_from(p.x) else {
+            return false;
+        };
+        let Ok(y) = usize::try_from(p.y) else {
+            return false;
+        };
+        x < self.w && y < self.h
+    }
+
+    pub fn all_points(&self) -> impl Iterator<Item = (Point, u8)> + '_ {
+        self.tiles.iter().enumerate().flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(x, c)| (Point::new(x as i64, y as i64), *c))
+        })
+    }
+
+    pub fn print(&self) {
+        for row in self.tiles.iter() {
+            for c in row {
+                let c = *c as char;
+                print!("{c}");
+            }
+            println!();
+        }
+    }
+}
