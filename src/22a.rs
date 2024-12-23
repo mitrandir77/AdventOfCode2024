fc1a6e58aaf71aa273eecddd9866cbc487d45f24
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-23 14:40:39 +0100

    Day 22

--- src/22a.rs
+++ src/22a.rs
@@ -4,9 +4,36 @@
 use anyhow::Result;
 use std::io::BufRead;
 
+fn next_secret_number(mut secret: u64) -> u64 {
+    secret = secret ^ (secret * 64);
+    secret %= 16777216;
+    secret = secret ^ (secret / 32);
+    secret %= 16777216;
+    secret = secret ^ (secret * 2048);
+    secret %= 16777216;
+    secret
+}
+
+fn nth_secret_number(mut seed: u64, n: u64) -> u64 {
+    for _i in 0..n {
+        seed = next_secret_number(seed);
+    }
+    seed
+}
+
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut res = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let seed = line.parse()?;
+        // dbg!(&seed);
+        let secret = nth_secret_number(seed, 2000);
+        // dbg!(&secret);
+        res += secret;
+    }
+    println!("{res}");
+
     Ok(())
 }
