d30df120d97386e89596dd7dbab600a32f6132fe
Mateusz Kwapich <mateusz@kwapich.it>
2024-12-24 01:16:46 +0100

    Day 23

--- src/23a.rs
+++ src/23a.rs
@@ -2,11 +2,41 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use multimap::MultiMap;
+use scan_rules::scanner::Word;
+use std::{collections::HashSet, io::BufRead};
+#[macro_use]
+extern crate scan_rules;
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut graph = MultiMap::new();
+    let mut edges = HashSet::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let_scan!(line; (let a: Word<String>, "-", let b: Word<String>));
+        graph.insert(a.clone(), b.clone());
+        graph.insert(b.clone(), a.clone());
+        edges.insert((a.clone(), b.clone()));
+        // edges.insert((b.clone(), a.clone()));
+    }
+
+    let mut k3s = HashSet::new();
+    for (a, b) in edges.iter() {
+        let a_edges: HashSet<&String> = graph.get_vec(a).unwrap().iter().collect();
+        let b_edges: HashSet<&String> = graph.get_vec(b).unwrap().iter().collect();
+        for c in a_edges.intersection(&b_edges) {
+            if a.starts_with("t") || b.starts_with("t") || c.starts_with("t") {
+                let mut k3 = vec![a.to_string(), b.to_string(), c.to_string()];
+                k3.sort();
+                k3s.insert(k3);
+            }
+        }
+    }
+
+    println!("{}", k3s.len());
+    // dbg!(k3s);
+
     Ok(())
 }
--- src/23b.rs
+++ src/23b.rs
@@ -2,11 +2,67 @@
 // (c) 2024 Mateusz Kwapich
 
 use anyhow::Result;
-use std::io::BufRead;
+use multimap::MultiMap;
+use scan_rules::scanner::Word;
+use std::{
+    collections::{BTreeMap, BTreeSet, HashSet},
+    hash::Hash,
+    io::BufRead,
+};
+#[macro_use]
+extern crate scan_rules;
+
+type Node = (u8, u8);
+type Graph = BTreeMap<Node, BTreeSet<Node>>;
+
+fn extend_clique(
+    graph: &Graph,
+    k: BTreeSet<Node>,
+    mut candidates: BTreeSet<Node>,
+    max: &mut BTreeSet<Node>,
+) {
+    for cand in candidates.clone().iter() {
+        let cand_edges = graph.get(cand).unwrap();
+        if cand_edges.intersection(&k).count() == k.len() {
+            let mut k = k.clone();
+            candidates.remove(cand);
+            k.insert(*cand);
+            if k.len() > max.len() {
+                *max = k.clone();
+            }
+            extend_clique(graph, k, candidates.clone(), max);
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
 
-    for _line in stdin.lock().lines() {}
+    let mut graph = BTreeMap::<Node, BTreeSet<Node>>::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let_scan!(line; (let a: Word<String>, "-", let b: Word<String>));
+        let a = (
+            a.chars().next().unwrap() as u8,
+            a.chars().nth(1).unwrap() as u8,
+        );
+        let b = (
+            b.chars().next().unwrap() as u8,
+            b.chars().nth(1).unwrap() as u8,
+        );
+        graph.entry(a).or_default().insert(b);
+        graph.entry(b).or_default().insert(a);
+    }
+
+    let mut max = BTreeSet::new();
+    let cand = graph.keys().cloned().collect();
+    extend_clique(&graph, BTreeSet::new(), cand, &mut max);
+    let mut max: Vec<String> = max
+        .into_iter()
+        .map(|(a, b)| format!("{}{}", a as char, b as char))
+        .collect();
+    max.sort();
+    println!("{}", max.join(","));
+
     Ok(())
 }
